bf7b728344b5677b684fe63e7fa73639e9bcd78a
t-denis <denis.timukhin@gmail.com>
2019-08-16 16:30:53 +0300

    fix SequenceEqual

--- dev/Modules/SequenceEqual.ts
+++ dev/Modules/SequenceEqual.ts
@@ -1,17 +1,18 @@
 Linq4JS.Helper.NonEnumerable("SequenceEqual", function<T> (this: T[], array: T[]): boolean {
-    if (this.Count() !== array.Count()) {
+    if (this === array) {
+        return true;
+    }
+    if (this == null || array == null) {
+        return false;
+    }
+    if (this.length !== array.length) {
         return false;
     }
 
     for (let i = 0; i < this.length; i++) {
-        let keys = Object.keys(this[i]);
-
-        for (let key of keys){
-            if ((this[i] as any)[key] !== (array[i] as any)[key]) {
-                return false;
-            }
+        if (this[i] !== array[i]) {
+            return false;
         }
     }
-
     return true;
 });
