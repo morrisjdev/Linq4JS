26ad0a9e7015d248a5322d7a000a18b5c67757a4
morrisjdev <morrisj@live.de>
2019-10-29 12:38:47 +0100

    fixed SequenceEqual.ts

--- dev/Modules/SequenceEqual.ts
+++ dev/Modules/SequenceEqual.ts
@@ -1,13 +1,34 @@
 Linq4JS.Helper.NonEnumerable("SequenceEqual", function<T> (this: T[], array: T[]): boolean {
-    if (this.Count() !== array.Count()) {
+    if (this === array) {
+        return true;
+    }
+
+    if (this == null || array == null) {
+        return false;
+    }
+
+    if (this.length !== array.length) {
         return false;
     }
 
     for (let i = 0; i < this.length; i++) {
-        let keys = Object.keys(this[i]);
+        const currentObjectThis: any = this[i];
+        const currentObjectArray: any = array[i];
 
-        for (let key of keys){
-            if ((this[i] as any)[key] !== (array[i] as any)[key]) {
+        if (currentObjectThis instanceof Array && currentObjectArray instanceof Array) {
+            if (!currentObjectThis.SequenceEqual(currentObjectArray)) {
+                return false;
+            }
+        } else if (currentObjectThis instanceof Object && currentObjectArray instanceof Object) {
+            let keys = Object.keys(currentObjectThis);
+
+            for (let key of keys){
+                if (currentObjectThis[key] !== currentObjectArray[key]) {
+                    return false;
+                }
+            }
+        } else {
+            if (currentObjectThis !== currentObjectArray) {
                 return false;
             }
         }
--- test/test.js
+++ test/test.js
@@ -435,6 +435,46 @@ describe("SequenceEqual", function () {
         expect(Users.SequenceEqual(Users.Skip(1))).toBeFalsy();
         expect(Users.SequenceEqual(Fruits)).toBeFalsy();
     });
+    it("numbers", function () {
+        var source1 = [1, 2, 3];
+        var source2 = [1, 2, 4];
+        expect(source1.SequenceEqual(source2)).toBeFalsy();
+    });
+    it("arrays", function () {
+        var source1 = [
+            [1, 2, 3],
+            [2, 3, 4]
+        ];
+        var source2 = [
+            [1, 2, 3],
+            [2, 3, 4]
+        ];
+        var source3 = [
+            {},
+            [2, 4, 4]
+        ];
+        expect(source1.SequenceEqual(source2)).toBeTruthy();
+        expect(source1.SequenceEqual(source3)).toBeFalsy();
+    });
+    it("objects", function () {
+        var source1 = [
+            { k1: 1, k2: 'test2', k3: false },
+            { k1: 2, k2: 'test5', k3: true },
+            { k1: 5, k2: 'test1', k3: false }
+        ];
+        var source2 = [
+            { k1: 1, k2: 'test2', k3: false },
+            { k1: 2, k2: 'test5', k3: true },
+            { k1: 5, k2: 'test1', k3: false }
+        ];
+        var source3 = [
+            { k1: 1, k2: 'test1', k3: false },
+            { k1: 6, k2: 'test5', k3: true },
+            { k1: 5, k2: 'test1', k3: false }
+        ];
+        expect(source1.SequenceEqual(source2)).toBeTruthy();
+        expect(source1.SequenceEqual(source3)).toBeFalsy();
+    });
 });
 describe("Single", function () {
     it("user", function () {
